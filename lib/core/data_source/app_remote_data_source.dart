01eb90f810aec6ec4442106662228761dad949e7
Muneeb Masood <muneebned844@gmail.com>
2026-05-06 22:55:49 +0500

    Update app_remote_data_source.dart

--- lib/core/data_source/app_remote_data_source.dart
+++ lib/core/data_source/app_remote_data_source.dart
@@ -66,8 +66,8 @@ class AppRemoteDataSource extends BaseRemoteDataSource {
       method: RequestType.post,
       headers: headers,
       data: formData,
-      sendTimeout: const Duration(seconds: 120),
-      receiveTimeout: const Duration(seconds: 120),
+      sendTimeout: const Duration(seconds: 480),
+      receiveTimeout: const Duration(seconds: 480),
       transformer: (data) =>
           EvalAnswerModel.fromJson(data as Map<String, dynamic>),
     );
